9573e42bdea99b03952a46f2d644d87f68c6deec
Batandwa Mgutsi <b.mgutsi@gmail.com>
2021-10-16 21:06:28 +0200

    Implements main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log("hello!");
+import { compileAssociations } from './cli_utils';
+import { TemplateRule } from './models/config_models';
+import AssociationsService from './services/associations_service';
+import { newBuildService } from './services/build_service';
+
+async function main() {
+  if (process.argv.length < 3) throw new Error('Too few arguments passed');
+  const associations = compileAssociations(process.argv.splice(2).join(' '));
+  const dbAssociations = await AssociationsService.instance.toNotionDatabaseAssociations(associations);
+
+  const configFile = `${process.cwd()}/motionlink.config.js`;
+  const templateRules: TemplateRule[] = require(configFile);
+
+  await newBuildService().build(templateRules, dbAssociations);
+}
+
+main();
